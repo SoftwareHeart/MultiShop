58e5c15f08c9021abbc725b1dae3aa55400bf20e
ERTUGRUL\ertug <ertugrulkundak@hotmail.com>
2025-01-14 00:26:33 +0300

    ProductDetail ve ProductImages entity'leri eklendi.

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,6 +1,13 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class ProductDetail
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
     }
 }
