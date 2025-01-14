562ccb2a5660c5c325e20ecee99b8044e547fbb6
Ertuğrul Kundak <ertugrulkundak@hotmail.com>
2025-01-14 23:32:34 +0300

    AutoMapper
    AutoMapper eklendi.

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -9,5 +9,9 @@ namespace MultiShop.Catalog.Entities
         public string ProductDetailID { get; set; }
         public string ProductDescription { get; set; }
         public string ProductInfo { get; set; }
+        public string ProductId { get; set; }
+
+        [BsonIgnore]
+        public Product Product { get; set; }
     }
 }
